485bf0f981ec1fea1b8fd289230425eca2ecb448
Anish Suman <62011810+anish877@users.noreply.github.com>
2025-04-23 11:40:25 +0530

    Update sendVerificationEmail.ts

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -24,7 +24,7 @@ export async function sendVerificationEmail(
         // Send mail with defined transport object
         await transporter.sendMail({
             from: 'anishsuman2305@gmail.com',
-            to: `sumananish40@gmail.com`,
+            to: `${email}`,
             subject: 'Verification code',
             html: emailHtml
         });
